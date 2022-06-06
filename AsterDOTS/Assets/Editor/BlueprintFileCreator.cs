7c49ba85728ed29b002f07e37904a53e71672544
Pit <pavelrasiukevich@gmail.com>
2022-06-06 17:45:57 +0500

    a little bit cleaner but still not perfect

--- AsterDOTS/Assets/Editor/BlueprintFileCreator.cs
+++ AsterDOTS/Assets/Editor/BlueprintFileCreator.cs
@@ -16,41 +16,49 @@ namespace Root
         {
             var selectedObjectInProjectHierarchyPath = SelectedObjectPathResolver.Resolve();
 
+            var validatedPath = ValidatePath(selectedObjectInProjectHierarchyPath);
+
             var containBlueprintName = Directory.GetFiles(Application.dataPath, "*.cs", SearchOption.AllDirectories)
                 .Where(f => f.Contains(fileName)).ToArray();
 
             if (containBlueprintName.Length > 0)
             {
                 var nameOfTheCopy = $"{fileName}{containBlueprintName.Length}";
-                CreateFile(Path.Combine(selectedObjectInProjectHierarchyPath, $"{nameOfTheCopy}{suffix}"));
+                CreateFile(Path.Combine(validatedPath, $"{nameOfTheCopy}{suffix}"));
                 RefreshHierarchy();
             }
             else
             {
-                CreateFile(Path.Combine(selectedObjectInProjectHierarchyPath, $"{fileName}{suffix}"));
+                CreateFile(Path.Combine(validatedPath, $"{fileName}{suffix}"));
                 RefreshHierarchy();
             }
+        }
+
+        private static string ValidatePath(string path)
+        {
+            var p = path;
+
+            if (Path.GetExtension(path).Length > 0)
+            {
+                var t = Directory.GetParent(path);
+                Selection.activeObject = null;
+                p = t.FullName;
+            }
+
+            return p;
         }
 
         private static void CreateFile(string path)
         {
             using (FileStream fs = File.Create(path))
             {
-                var content = $"using Unity.Entities;\n\n[GenerateAuthoringComponent]\npublic struct {GetFileNameFromPath(path)} : IComponentData\n{{\n}}\n";
+                var content = $"using Unity.Entities;\n\n[GenerateAuthoringComponent]\npublic struct {GetFileName(path)} : IComponentData\n{{\n}}\n";
                 byte[] info = new UTF8Encoding(true).GetBytes(content);
                 fs.Write(info, 0, info.Length);
             }
         }
 
-        private static string GetFileNameFromPath(string path)
-        {
-            var start = path.IndexOf("\\");
-            var end = path.IndexOf(".");
-            start += 1;
-            var length = end - start;
-
-            return path.Substring(start, length);
-        }
+        private static string GetFileName(string path) => Path.GetFileNameWithoutExtension(path);
 
         private static void RefreshHierarchy() => AssetDatabase.Refresh();
     }
